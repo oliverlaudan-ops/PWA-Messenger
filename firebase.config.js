30a4c13243de34cde5a6322246d708bf46270b1b
Oliver <oliver.laudan@gmail.com>
2026-02-17 14:20:06 +0100

    Add Firebase configuration file

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+// firebase.config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBlZRSrjkYYP8KtycZOah8fX-RMMnYYPj4",
+  authDomain: "pwa-messenger-oliver.firebaseapp.com",
+  projectId: "pwa-messenger-oliver",
+  storageBucket: "pwa-messenger-oliver.firebasestorage.app",
+  messagingSenderId: "171952836516",
+  appId: "1:171952836516:web:427a7829345cde6ed8fb31",
+  measurementId: "G-0w21RL0G06"
+};
